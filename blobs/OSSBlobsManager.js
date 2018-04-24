4c6b36a0afe61b271c6c68b91ed0005cc8750590
Julien Sarazin <julien.sarazin@me.com>
2018-04-25 00:21:34 +0200

    assets creation does not returns a promise anynore

--- blobs/OSSBlobsManager.js
+++ blobs/OSSBlobsManager.js
@@ -32,7 +32,7 @@ class OSSBlobsManager {
     /**
      *
      * @param blobs, the blobs to create. expected format is: {name: String, path: String}
-     * @returns {*} a Promise containing {name: String, path: String, upload_url: String}
+     * @returns an array containing {name: String, path: String, upload_url: String}
      */
     create(blobs) {
         const self = this;
@@ -43,10 +43,9 @@ class OSSBlobsManager {
         const hasInvalidBlobs = blobs.some(blob => !blob.name || !blob.path);
 
         if (hasInvalidBlobs)
-            return Promise.reject(new Error('some.invalid.blobs'));
+            throw new Error('some.invalid.blobs');
 
-        return Promise
-            .map(blobs, create);
+        return blobs.map(create);
 
         function create(blob) {
             blob.upload_url = self.client.signatureUrl(blob.path, self.configuration.signature);
